a2ff1457b9d4437a36ba6ecb4661b908ff64b494
James <jamesnjunge45@gmail.com>
2023-04-05 15:39:06 +0300

    first commit

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [Required]
+        [DisplayName("Account Number")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("Bank Name")]
+        [Required]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("SWIFT Code")]
+        [Required]
+        public string SWIFTCode { get; set; }
+        
+
+        public int Amount { get; set;}
+
+     
+        public DateTime Date { get; set; }
+
+      
+    }
+}
---
+++ Expense Tracker/Models/TransactionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options) 
+        {}
+
+        public DbSet<Transaction>Transactions { get; set; }
+
+    }
+}
